dd6b03b4ce7fc3b7a22a090089967674934bc0c3
LOLPANIC <113506270+LOLPANIC@users.noreply.github.com>
2024-09-14 23:20:56 +0300

    Update ore_pool.sh

--- ore_pool.sh
+++ ore_pool.sh
@@ -16,48 +16,48 @@ echo "--------------------------------------------------------------"
 
 # Function to show the menu
 show_menu() {
-    echo "请选择一个选项，请用root用户操作："
-    echo "1. 一键挖矿"
-    echo "2. 查看挖矿状态"
-    echo "3. 一键领取奖励"
-    echo "4. 停止挖矿"
-    echo "5. 退出"
-    echo -n "输入选项 [1-5]: "
+    echo "Пожалуйста, выберите опцию, пожалуйста, используйте root пользователя для работы:"
+    echo "1. quick"
+    echo "2. log"
+    echo "3. balance"
+    echo "4. stop"
+    echo "5. exit"
+    echo -n "enter [1-5]: "
 }
 
 # Function to start mining
 start_mining() {
-    echo "开始一键挖矿..."
-    read -p "请输入线程数: " threads
-    read -p "请输入ore钱包地址: " address
+    echo "Начните с одного из них..."
+    read -p "Пожалуйста, введите количество потоков: " threads
+    read -p "Пожалуйста, введите адрес рудного кошелька ore: " address
     apt update -y
     apt install screen -y
     pkill -9 screen
     screen -wipe
 
     # Start mining in the background and redirect output to ~/output.log
-    screen -S ore-miner ~/ore-pool/ore-miner  mine --address "$address" --threads "$threads"  --invcode 2QKLTH
+    screen -S ore-miner ~/ore-pool/ore-miner  mine --address "$address" --threads "$threads"  --invcode IUSEP7
 }
 
 
 
 # Function to check mining status
 check_mining_status() {
-    echo "查看挖矿状态..."
+    echo "Посмотреть статус добычи ....."
     screen -r ore-miner
 }
 
 # Function to claim rewards
 claim_rewards() {
-    echo "一键领取奖励..."
-    read -p "请输入ore钱包地址: " address
-    ~/ore-pool/ore-miner  claim --address "$address" --invcode 2QKLTH
+    echo "Ключевая получение награды..."
+    read -p "Пожалуйста, введите адрес рудного кошелька: " address
+    ~/ore-pool/ore-miner  claim --address "$address" --invcode IUSEP7
 }
 
 
 # Function to stop mining
 stop_mining() {
-    echo "停止挖矿..."
+    echo "Перестань добывать ..."
     pkill -9 screen
 	screen -wipe
 }
@@ -81,11 +81,11 @@ while true; do
             stop_mining
             ;;
         5)
-            echo "退出脚本..."
+            echo "Выйдите из сценария ..."
             break
             ;;
         *)
-            echo "无效的选项，请重试..."
+            echo "Если неверный вариант, попробуйте ..."
             ;;
     esac
 done
